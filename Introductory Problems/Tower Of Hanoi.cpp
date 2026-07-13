e65390e78e5d5be2fb714f3304d26dc40371b304
KhoaL85 <trongkhoa080509@gmail.com>
2026-07-13 09:19:01 +0700

    cleanse code

--- Introductory Problems/Tower Of Hanoi.cpp
+++ Introductory Problems/Tower Of Hanoi.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#define f first
+#define se second
 
 using namespace std;
 int n, k;
 vector<pair<int,int>>a;
-void solve(int n, int l, int m, int r)
+void solve(int n, int l, int r, int m)
 {
     if(n==1)
     {
         a.push_back({l,r});
         return;
     }
-    solve(n-1,l,r,m);
+    solve(n-1,l,m,r);
     a.push_back({l,r});
-    solve(n-1,m,l,r);
+    solve(n-1,m,r,l);
 }
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
     cin>>n;
-    solve(n,1,2,3);
-    k=a.size();
-    cout<<k<<' ';
-    for(int i=0;i<k;i++)
-        cout<<a[i].first<<' '<<a[i].second<<'\n';
+    solve(n,1,3,2);
+    cout<<a.size();
+    for(auto &x:a)
+        cout<<'\n'<<x.f<<' '<<x.se;
     return 0;
 }
